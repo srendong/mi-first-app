b8621ab82cc8da2d23c86147a1dd6ff36478e18a
Samuel Rendon G <s.redon.g@gmail.com>
2019-03-21 19:56:37 -0500

    mifirstapp2

---
+++ db/migrate/20190322003630_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.time :duration
+      t.date :year
+      t.string :rating
+      t.text :description
+      t.reference :image_url
+
+      t.timestamps
+    end
+  end
+end
